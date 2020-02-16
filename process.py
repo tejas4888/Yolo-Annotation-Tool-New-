ddb940cfa92b2628acec1179d28a37573634fcfa
Tejas Chheda <tejas4888@gmail.com>
2020-02-16 21:31:16 +0530

    Modified for categorical classes

--- process.py
+++ process.py
@@ -1,11 +1,11 @@
 import glob, os
 
 # Current directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
+#current_dir = os.path.dirname(os.path.abspath(__file__))
 
-print(current_dir)
+#print(current_dir)
 
-current_dir = 'images'
+#current_dir = 'images/cat'
 
 # Directory where the data will reside, relative to 'darknet.exe'
 #path_data = './NFPAdataset/'
@@ -20,12 +20,15 @@ file_test = open('valid.txt', 'w')
 # Populate train.txt and test.txt
 counter = 1
 index_test = 0 #round(100 / percentage_test) ==> Uncomment if you need test set
-for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
-    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+classes=os.listdir('images')
+for current_dir in classes:
+	current_dir = 'images/'+current_dir
+	for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
+	    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
 
-    if counter == index_test:
-        counter = 1
-        file_test.write("data/custom/"+current_dir + "/" + title + '.jpg' + "\n")
-    else:
-        file_train.write("data/custom/"+current_dir + "/" + title + '.jpg' + "\n")
-        counter = counter + 1
+	    if counter == index_test:
+	        counter = 1
+	        file_test.write("data/custom/"+current_dir + "/" + title + '.jpg' + "\n")
+	    else:
+	        file_train.write("data/custom/"+current_dir + "/" + title + '.jpg' + "\n")
+	        counter = counter + 1
